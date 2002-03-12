42981ecfd71495a44397987b668cd513fa495083
Scott Klement <tn5250@scottklement.com>
2002-03-12 06:12:11 +0000

    Applied James Rich' patch to centralize some of the SCS definitions, and fix some bugs in scs2pdf.

--- src/scs2pdf.c
+++ src/scs2pdf.c
@@ -21,44 +21,11 @@
  */
 
 #include "tn5250-private.h"
+#include "scs.h"
 #include <glib.h>
 
-static void scs2ascii_sic ();
-static void scs2ascii_sil ();
-static void scs2ascii_sls ();
-int scs2ascii_process34 (int *curpos);
-static void scs2ascii_process2b ();
-static void scs2ascii_rff ();
-static void scs2ascii_noop ();
-static void scs2ascii_transparent ();
-static void scs2ascii_processd2 ();
-static void scs2ascii_process03 ();
-static void scs2ascii_scs ();
-static void scs2ascii_ssld ();
-static void scs2ascii_process04 ();
-static void scs2ascii_processd1 ();
-static void scs2ascii_process06 ();
-static void scs2ascii_process07 ();
-static void scs2ascii_processd103 ();
-static void scs2ascii_stab ();
-static void scs2ascii_jtf ();
-static void scs2ascii_sjm ();
-static void scs2ascii_spps ();
-static void scs2ascii_ppm ();
-static void scs2ascii_svm ();
-static void scs2ascii_spsu ();
-static void scs2ascii_sea ();
-static void scs2ascii_shm ();
-static void scs2ascii_sgea ();
-static void scs2ascii_cr ();
-static void scs2ascii_nl ();
-static void scs2ascii_rnl ();
-static void scs2ascii_ht ();
-int scs2ascii_ahpp (int *curpos);
-static void scs2ascii_avpp ();
-static void scs2ascii_processd3 ();
-static void scs2ascii_sto ();
-static void scs2ascii_ff ();
+int scs2pdf_process34 (int *curpos);
+int scs2pdf_ahpp (int *curpos);
 
 int pdf_header ();
 int pdf_catalog (int objnum, int outlinesobject, int pageobject);
@@ -67,7 +34,8 @@ int pdf_begin_stream (int objnum);
 int pdf_end_stream ();
 int pdf_stream_length (int objnum, int objlength);
 int pdf_pages (int objnum, int pagechildren, int pages);
-int pdf_page (int objnum, int parent, int contents, int procset, int font);
+int pdf_page (int objnum, int parent, int contents, int procset, int font,
+	      int pagewidth, int pagelength);
 int pdf_procset (int objnum);
 int pdf_font (int objnum);
 void pdf_xreftable (int objnum);
@@ -76,8 +44,6 @@ int pdf_process_char (char character, int flush);
 
 unsigned char curchar;
 unsigned char nextchar;
-int new_line;
-int pagewidth = 612, pagelength = 792;
 
 GArray *ObjectList;
 
@@ -87,6 +53,7 @@ int
 main ()
 {
   Tn5250CharMap *map = tn5250_char_map_new ("37");
+  int pagewidth = 612, pagelength = 792;
   int objcount = 0;
   int filesize = 0;
   int streamsize = 0;
@@ -96,8 +63,8 @@ main ()
   int i;
   int newpage = 0;
   int column;
+  int new_line = 1;
   ObjectList = g_array_new (FALSE, FALSE, sizeof (int));
-  new_line = 1;
 
   if ((getenv ("TN5250_PDF")) != NULL)
     {
@@ -132,53 +99,53 @@ main ()
 	{
 	case SCS_TRANSPARENT:
 	  {
-	    scs2ascii_transparent ();
+	    scs_transparent ();
 	    break;
 	  }
 	case SCS_RFF:
 	  {
-	    scs2ascii_rff ();
+	    scs_rff ();
 	    break;
 	  }
 	case SCS_NOOP:
 	  {
-	    scs2ascii_noop ();
+	    scs_noop ();
 	    break;
 	  }
 	case SCS_CR:
 	  {
-	    scs2ascii_cr ();
+	    scs_cr ();
 	    break;
 	  }
 	case SCS_FF:
 	  {
-	    scs2ascii_ff ();
+	    scs_ff ();
 	    newpage = 1;
 	    break;
 	  }
 	case SCS_NL:
 	  {
-	    scs2ascii_nl ();
+	    new_line = scs_nl ();
 	    break;
 	  }
 	case SCS_RNL:
 	  {
-	    scs2ascii_rnl ();
+	    scs_rnl ();
 	    break;
 	  }
 	case SCS_HT:
 	  {
-	    scs2ascii_ht ();
+	    scs_ht ();
 	    break;
 	  }
 	case 0x34:
 	  {
-	    streamsize += scs2ascii_process34 (&column);
+	    streamsize += scs2pdf_process34 (&column);
 	    break;
 	  }
 	case 0x2B:
 	  {
-	    scs2ascii_process2b ();
+	    scs_process2b (&pagewidth, &pagelength);
 	    break;
 	  }
 	case 0xFF:
@@ -290,7 +257,8 @@ main ()
       objcount++;
       filesize += pdf_page (objcount,
 			    pageparent,
-			    textobjects[i], procsetobject, fontobject);
+			    textobjects[i], procsetobject, fontobject,
+			    pagewidth, pagelength);
 #ifdef DEBUG
       fprintf (stderr, "page objcount = %d\n", objcount);
 #endif
@@ -304,72 +272,8 @@ main ()
   return (0);
 }
 
-static void
-scs2ascii_ht ()
-{
-  fprintf (stderr, "HT\n");
-}
-
-static void
-scs2ascii_rnl ()
-{
-  fprintf (stderr, "RNL\n");
-}
-
-static void
-scs2ascii_nl ()
-{
-  new_line = 1;
-#ifdef DEBUG
-  fprintf (stderr, "NL\n");
-#endif
-}
-
-static void
-scs2ascii_ff ()
-{
-#ifdef DEBUG
-  fprintf (stderr, "FF\n");
-#endif
-}
-
-static void
-scs2ascii_cr ()
-{
-  fprintf (stderr, "CR\n");
-}
-
-static void
-scs2ascii_transparent ()
-{
-
-  int bytecount;
-  int loop;
-
-  bytecount = fgetc (stdin);
-  fprintf (stderr, "TRANSPARENT (%x) = ", bytecount);
-  for (loop = 0; loop < bytecount; loop++)
-    {
-      /*printf("%c", fgetc(stdin)); */
-    }
-}
-
-static void
-scs2ascii_rff ()
-{
-  fprintf (stderr, "RFF\n");
-}
-
-static void
-scs2ascii_noop ()
-{
-#ifdef DEBUG
-  fprintf (stderr, "NOOP\n");
-#endif
-}
-
 int
-scs2ascii_process34 (int *curpos)
+scs2pdf_process34 (int *curpos)
 {
   int bytes;
 
@@ -379,12 +283,12 @@ scs2ascii_process34 (int *curpos)
     {
     case SCS_AVPP:
       {
-	scs2ascii_avpp ();
+	scs_avpp ();
 	break;
       }
     case SCS_AHPP:
       {
-	bytes = scs2ascii_ahpp (curpos);
+	bytes = scs2pdf_ahpp (curpos);
 	break;
       }
     default:
@@ -395,14 +299,8 @@ scs2ascii_process34 (int *curpos)
   return (bytes);
 }
 
-static void
-scs2ascii_avpp ()
-{
-  fprintf (stderr, "AVPP %d\n", fgetc (stdin));
-}
-
 int
-scs2ascii_ahpp (int *curpos)
+scs2pdf_ahpp (int *curpos)
 {
   int position, bytes;
   int i;
@@ -416,10 +314,10 @@ scs2ascii_ahpp (int *curpos)
        * all screwed up when we do.  Since it looks correct without the
        * new line I'm going to leave it out. */
       /*
-      bytes += pdf_process_char ('\0', 1);
-      fprintf (outfile, "0 -12 Td\n");
-      bytes += 9;
-      */
+         bytes += pdf_process_char ('\0', 1);
+         fprintf (outfile, "0 -12 Td\n");
+         bytes += 9;
+       */
 
       for (i = 0; i < position; i++)
 	{
@@ -441,452 +339,6 @@ scs2ascii_ahpp (int *curpos)
   return (bytes);
 }
 
-static void
-scs2ascii_process2b ()
-{
-
-  curchar = fgetc (stdin);
-  switch (curchar)
-    {
-    case 0xD1:
-      {
-	scs2ascii_processd1 ();
-	break;
-      }
-    case 0xD2:
-      {
-	scs2ascii_processd2 ();
-	break;
-      }
-    case 0xD3:
-      {
-	scs2ascii_processd3 ();
-	break;
-      }
-    case 0xC8:
-      {
-	scs2ascii_sgea ();
-	break;
-      }
-    default:
-      {
-	fprintf (stderr, "ERROR: Unknown 0x2B command %x\n", curchar);
-      }
-    }
-}
-
-static void
-scs2ascii_processd3 ()
-{
-  curchar = fgetc (stdin);
-  nextchar = fgetc (stdin);
-
-  if (nextchar == 0xF6)
-    {
-      scs2ascii_sto ();
-    }
-  else
-    {
-      fprintf (stderr, "ERROR: Unknown 0x2BD3 %x %x", curchar, nextchar);
-    }
-}
-
-static void
-scs2ascii_sto ()
-{
-  int loop;
-
-  fprintf (stderr, "STO = ");
-  for (loop = 0; loop < curchar - 2; loop++)
-    {
-      nextchar = fgetc (stdin);
-      fprintf(stderr, " %x", nextchar);
-    }
-  fprintf (stderr, "\n");
-}
-
-static void
-scs2ascii_sgea ()
-{
-  fprintf (stderr, "SGEA = %x %x %x\n", fgetc (stdin), fgetc (stdin),
-	   fgetc (stdin));
-}
-
-static void
-scs2ascii_processd1 ()
-{
-  curchar = fgetc (stdin);
-  switch (curchar)
-    {
-    case 0x06:
-      {
-	scs2ascii_process06 ();
-	break;
-      }
-    case 0x07:
-      {
-	scs2ascii_process07 ();
-	break;
-      }
-    case 0x03:
-      {
-	scs2ascii_processd103 ();
-	break;
-      }
-    default:
-      {
-	fprintf (stderr, "ERROR: Unknown 0x2BD1 command %x\n", curchar);
-      }
-    }
-}
-
-static void
-scs2ascii_process06 ()
-{
-  curchar = fgetc (stdin);
-  if (curchar == 0x01)
-    {
-      fprintf (stderr, "GCGID = %x %x %x %x\n", fgetc (stdin), fgetc (stdin),
-	       fgetc (stdin), fgetc (stdin));
-    }
-  else
-    {
-      fprintf (stderr, "ERROR: Unknown 0x2BD106 command %x\n", curchar);
-    }
-}
-
-static void
-scs2ascii_process07 ()
-{
-  curchar = fgetc (stdin);
-  if (curchar == 0x05)
-    {
-      fprintf (stderr, "FID = %x %x %x %x %x\n", fgetc (stdin), fgetc (stdin),
-	       fgetc (stdin), fgetc (stdin), fgetc (stdin));
-    }
-  else
-    {
-      fprintf (stderr, "ERROR: Unknown 0x2BD107 command %x\n", curchar);
-    }
-}
-
-static void
-scs2ascii_processd103 ()
-{
-  curchar = fgetc (stdin);
-  if (curchar == 0x81)
-    {
-      fprintf (stderr, "SCGL = %x\n", fgetc (stdin));
-    }
-  else
-    {
-      fprintf (stderr, "ERROR: Unknown 0x2BD103 command %x\n", curchar);
-    }
-}
-
-static void
-scs2ascii_processd2 ()
-{
-  curchar = fgetc (stdin);
-  nextchar = fgetc (stdin);
-
-
-  switch (nextchar)
-    {
-    case 0x01:
-      {
-	scs2ascii_stab ();
-	break;
-      }
-    case 0x03:
-      {
-	scs2ascii_jtf ();
-	break;
-      }
-    case 0x0D:
-      {
-	scs2ascii_sjm ();
-	break;
-      }
-    case 0x40:
-      {
-	scs2ascii_spps ();
-	break;
-      }
-    case 0x48:
-      {
-	scs2ascii_ppm ();
-	break;
-      }
-    case 0x49:
-      {
-	scs2ascii_svm ();
-	break;
-      }
-    case 0x4c:
-      {
-	scs2ascii_spsu ();
-	break;
-      }
-    case 0x85:
-      {
-	scs2ascii_sea ();
-	break;
-      }
-    case 0x11:
-      {
-	scs2ascii_shm ();
-	break;
-      }
-    default:
-      {
-	switch (curchar)
-	  {
-	  case 0x03:
-	    {
-	      scs2ascii_process03 ();
-	      break;
-	    }
-	  case 0x04:
-	    {
-	      scs2ascii_process04 ();
-	      break;
-	    }
-	  default:
-	    {
-	      fprintf (stderr, "ERROR: Unknown 0x2BD2 command %x\n", curchar);
-	    }
-	  }
-      }
-    }
-
-}
-
-static void
-scs2ascii_stab ()
-{
-  int loop;
-
-  fprintf (stderr, "STAB = ");
-  for (loop = 0; loop < curchar - 2; loop++)
-    {
-      nextchar = fgetc (stdin);
-      fprintf (stderr, " %x", nextchar);
-    }
-  fprintf (stderr, "\n");
-}
-
-static void
-scs2ascii_jtf ()
-{
-  int loop;
-
-  fprintf (stderr, "JTF = ");
-  for (loop = 0; loop < curchar - 2; loop++)
-    {
-      nextchar = fgetc (stdin);
-      fprintf (stderr, " %x", nextchar);
-    }
-  fprintf (stderr, "\n");
-}
-
-static void
-scs2ascii_sjm ()
-{
-  int loop;
-
-  fprintf (stderr, "SJM = ");
-  for (loop = 0; loop < curchar - 2; loop++)
-    {
-      nextchar = fgetc (stdin);
-      fprintf (stderr, " %x", nextchar);
-    }
-  fprintf (stderr, "\n");
-}
-
-static void
-scs2ascii_spps ()
-{
-  int width, length;
-
-  fprintf (stderr, "SPPS = ");
-
-  width = fgetc (stdin);
-  width = (width << 8) + fgetc (stdin);
-  pagewidth = width / 1440 * 72;
-
-  length = fgetc (stdin);
-  length = (length << 8) + fgetc (stdin);
-  pagelength = length / 1440 * 72;
-
-  fprintf (stderr, "SPPS (width = %d) (length = %d)\n", width, length);
-
-}
-
-static void
-scs2ascii_ppm ()
-{
-  int loop;
-
-  fprintf (stderr, "PPM = ");
-  for (loop = 0; loop < curchar - 2; loop++)
-    {
-      nextchar = fgetc (stdin);
-      fprintf (stderr, " %x", nextchar);
-    }
-  fprintf (stderr, "\n");
-}
-
-
-static void
-scs2ascii_svm ()
-{
-  int loop;
-
-  fprintf (stderr, "SVM = ");
-  for (loop = 0; loop < curchar - 2; loop++)
-    {
-      nextchar = fgetc (stdin);
-      fprintf (stderr, " %x", nextchar);
-    }
-  fprintf (stderr, "\n");
-}
-
-static void
-scs2ascii_spsu ()
-{
-  int loop;
-
-  fprintf (stderr, "SPSU (%x) = ", curchar);
-  for (loop = 0; loop < curchar - 2; loop++)
-    {
-      nextchar = fgetc (stdin);
-      fprintf (stderr, " %x", nextchar);
-    }
-  fprintf (stderr, "\n");
-}
-
-static void
-scs2ascii_sea ()
-{
-  int loop;
-
-  fprintf (stderr, "SEA (%x) = ", curchar);
-  for (loop = 0; loop < curchar - 2; loop++)
-    {
-      nextchar = fgetc (stdin);
-      fprintf (stderr, " %x", nextchar);
-    }
-  fprintf (stderr, "\n");
-}
-
-static void
-scs2ascii_shm ()
-{
-  int loop;
-
-  fprintf (stderr, "SHM = ");
-  for (loop = 0; loop < curchar - 2; loop++)
-    {
-      nextchar = fgetc (stdin);
-      fprintf (stderr, " %x", nextchar);
-    }
-  fprintf (stderr, "\n");
-}
-
-static void
-scs2ascii_process03 ()
-{
-  switch (nextchar)
-    {
-    case 0x45:
-      {
-	scs2ascii_sic ();
-	break;
-      }
-    case 0x07:
-      {
-	scs2ascii_sil ();
-	break;
-      }
-    case 0x09:
-      {
-	scs2ascii_sls ();
-	break;
-      }
-    default:
-      {
-	fprintf (stderr, "ERROR: Unknown 0x2BD203 command %x\n", curchar);
-      }
-    }
-}
-
-static void
-scs2ascii_sic ()
-{
-  curchar = fgetc (stdin);
-  fprintf (stderr, "SIC = %x\n", curchar);
-}
-
-static void
-scs2ascii_sil ()
-{
-  curchar = fgetc (stdin);
-  fprintf (stderr, "SIL = %d", curchar);
-}
-
-static void
-scs2ascii_sls ()
-{
-  curchar = fgetc (stdin);
-  fprintf (stderr, "SLS = %d\n", curchar);
-}
-
-static void
-scs2ascii_process04 ()
-{
-  switch (nextchar)
-    {
-    case 0x15:
-      {
-	scs2ascii_ssld ();
-	break;
-      }
-    case 0x29:
-      {
-	scs2ascii_scs ();
-	break;
-      }
-    default:
-      {
-	fprintf (stderr, "ERROR: Unknown 0x2BD204 command %x\n", curchar);
-      }
-    }
-}
-
-static void
-scs2ascii_ssld ()
-{
-  curchar = fgetc (stdin);
-  nextchar = fgetc (stdin);
-  fprintf (stderr, "SSLD = %d %d \n", curchar, nextchar);
-}
-
-static void
-scs2ascii_scs ()
-{
-  curchar = fgetc (stdin);
-  if (curchar == 0x00)
-    {
-      curchar = fgetc (stdin);
-      fprintf (stderr, "SCS = %d", curchar);
-    }
-  else
-    {
-      fprintf (stderr, "ERROR: Unknown 0x2BD20429 command %x\n", curchar);
-    }
-}
-
 int
 pdf_header ()
 {
@@ -903,14 +355,12 @@ pdf_catalog (int objnum, int outlinesobject, int pageobject)
   char text[255];
 
   sprintf (text,
-	   "%d 0 obj\n" \
-	   "\t<<\n" \
-	   "\t\t/Type /Catalog\n" \
-	   "\t\t/Outlines %d 0 R\n" \
-	   "\t\t/Pages %d 0 R\n" \
-	   "\t>>\n" \
-	   "endobj\n\n",
-	   objnum, outlinesobject, pageobject);
+	   "%d 0 obj\n"
+	   "\t<<\n"
+	   "\t\t/Type /Catalog\n"
+	   "\t\t/Outlines %d 0 R\n"
+	   "\t\t/Pages %d 0 R\n"
+	   "\t>>\n" "endobj\n\n", objnum, outlinesobject, pageobject);
 
   fprintf (outfile, "%s", text);
 
@@ -923,13 +373,10 @@ pdf_outlines (int objnum)
   char text[255];
 
   sprintf (text,
-	   "%d 0 obj\n" \
-	   "\t<<\n" \
-	   "\t\t/Type Outlines\n" \
-	   "\t\t/Count 0\n" \
-	   "\t>>\n" \
-	   "endobj\n\n",
-	   objnum);
+	   "%d 0 obj\n"
+	   "\t<<\n"
+	   "\t\t/Type Outlines\n"
+	   "\t\t/Count 0\n" "\t>>\n" "endobj\n\n", objnum);
 
   fprintf (outfile, "%s", text);
 
@@ -942,15 +389,12 @@ pdf_begin_stream (int objnum)
   char text[255];
 
   sprintf (text,
-	   "%d 0 obj\n" \
-	   "\t<<\n" \
-	   "\t\t/Length %d 0 R\n" \
-	   "\t>>\n" \
-	   "stream\n" \
-	   "\tBT\n" \
-	   "\t\t/F1 10 Tf\n" \
-	   "\t\t36 756 Td\n",
-	   objnum, objnum + 1);
+	   "%d 0 obj\n"
+	   "\t<<\n"
+	   "\t\t/Length %d 0 R\n"
+	   "\t>>\n"
+	   "stream\n"
+	   "\tBT\n" "\t\t/F1 10 Tf\n" "\t\t36 756 Td\n", objnum, objnum + 1);
 
   fprintf (outfile, "%s", text);
 
@@ -960,9 +404,7 @@ pdf_begin_stream (int objnum)
 int
 pdf_end_stream ()
 {
-  char *text = "\tET\n" \
-    "endstream\n" \
-    "endobj\n\n";
+  char *text = "\tET\n" "endstream\n" "endobj\n\n";
 
   fprintf (outfile, "%s", text);
 
@@ -978,11 +420,7 @@ pdf_stream_length (int objnum, int objlength)
    * and pdf_end_stream() add to the stream.  These functions already add
    * to the filesize correctly so we don't add 32 to filesize anywhere.
    */
-  sprintf (text,
-	   "%d 0 obj\n" \
-	   "\t%d\n" \
-	   "endobj\n\n",
-	   objnum, objlength + 32);
+  sprintf (text, "%d 0 obj\n" "\t%d\n" "endobj\n\n", objnum, objlength + 32);
 
   fprintf (outfile, "%s", text);
 
@@ -997,32 +435,20 @@ pdf_pages (int objnum, int pagechildren, int pages)
   int i;
 
   sprintf (text,
-	   "%d 0 obj\n" \
-	   "\t<<\n" \
-	   "\t\t/Type /Pages\n" \
-	   "\t\t/Kids [",
-	   objnum);
+	   "%d 0 obj\n" "\t<<\n" "\t\t/Type /Pages\n" "\t\t/Kids [", objnum);
   fprintf (outfile, "%s", text);
   bytes = strlen (text);
-  sprintf (text,
-	   " %d 0 R\n",
-	   pagechildren);
+  sprintf (text, " %d 0 R\n", pagechildren);
   fprintf (outfile, "%s", text);
   bytes += strlen (text);
   for (i = 1; i < pages; i++)
     {
-      sprintf (text,
-	       "\t\t      %d 0 R\n",
-	       pagechildren + i);
+      sprintf (text, "\t\t      %d 0 R\n", pagechildren + i);
       fprintf (outfile, "%s", text);
       bytes += strlen (text);
     }
   sprintf (text,
-	   "\t\t      ]\n" \
-	   "\t\t/Count %d\n" \
-	   "\t>>\n" \
-	   "endobj\n\n",
-	   pages);
+	   "\t\t      ]\n" "\t\t/Count %d\n" "\t>>\n" "endobj\n\n", pages);
 
   fprintf (outfile, "%s", text);
   bytes += strlen (text);
@@ -1031,7 +457,8 @@ pdf_pages (int objnum, int pagechildren, int pages)
 }
 
 int
-pdf_page (int objnum, int parent, int contents, int procset, int font)
+pdf_page (int objnum, int parent, int contents, int procset, int font,
+	  int pagewidth, int pagelength)
 {
   char text[255];
 
@@ -1039,21 +466,22 @@ pdf_page (int objnum, int parent, int contents, int procset, int font)
    * You can set MediaBox to [0 0 612 792] to force this
    */
   sprintf (text,
-	   "%d 0 obj\n" \
-	   "\t<<\n" \
-	   "\t\t/Type /Page\n" \
-	   "\t\t/Parent %d 0 R\n" \
-	   "\t\t/MediaBox [0 0 %d %d]\n" \
-	   "\t\t/Contents %d 0 R\n" \
-	   "\t\t/Resources\n" \
-	   "\t\t\t<<\n" \
-	   "\t\t\t\t/ProcSet %d 0 R\n" \
-	   "\t\t\t\t/Font\n" \
-	   "\t\t\t\t\t<< /F1 %d 0 R >>\n" \
-	   "\t\t\t>>\n" \
-	   "\t>>\n" \
+	   "%d 0 obj\n"
+	   "\t<<\n"
+	   "\t\t/Type /Page\n"
+	   "\t\t/Parent %d 0 R\n"
+	   "\t\t/MediaBox [0 0 %d %d]\n"
+	   "\t\t/Contents %d 0 R\n"
+	   "\t\t/Resources\n"
+	   "\t\t\t<<\n"
+	   "\t\t\t\t/ProcSet %d 0 R\n"
+	   "\t\t\t\t/Font\n"
+	   "\t\t\t\t\t<< /F1 %d 0 R >>\n"
+	   "\t\t\t>>\n"
+	   "\t>>\n"
 	   "endobj\n\n",
-	   objnum, parent, pagewidth, pagelength, contents, procset, font);
+	   objnum, parent, (pagewidth / 1440 * 72), (pagelength / 1440 * 72),
+	   contents, procset, font);
 
   fprintf (outfile, "%s", text);
 
@@ -1065,11 +493,7 @@ pdf_procset (int objnum)
 {
   char text[255];
 
-  sprintf (text,
-	   "%d 0 obj\n" \
-	   "\t[/PDF /Text]\n" \
-	   "endobj\n\n",
-	   objnum);
+  sprintf (text, "%d 0 obj\n" "\t[/PDF /Text]\n" "endobj\n\n", objnum);
 
   fprintf (outfile, "%s", text);
 
@@ -1082,16 +506,13 @@ pdf_font (int objnum)
   char text[255];
 
   sprintf (text,
-	   "%d 0 obj\n" \
-	   "\t<<\n" \
-	   "\t\t/Type /Font\n" \
-	   "\t\t/Subtype /Type1\n" \
-	   "\t\t/Name /F1\n" \
-	   "\t\t/BaseFont /Courier\n" \
-	   "\t\t/Encoding /MacRomanEncoding\n" \
-	   "\t>>\n" \
-	   "endobj\n\n",
-	   objnum);
+	   "%d 0 obj\n"
+	   "\t<<\n"
+	   "\t\t/Type /Font\n"
+	   "\t\t/Subtype /Type1\n"
+	   "\t\t/Name /F1\n"
+	   "\t\t/BaseFont /Courier\n"
+	   "\t\t/Encoding /MacRomanEncoding\n" "\t>>\n" "endobj\n\n", objnum);
 
   fprintf (outfile, "%s", text);
 
@@ -1130,8 +551,7 @@ pdf_xreftable (int objnum)
   for (curobj = 0; curobj < objnum; curobj++)
     {
       fprintf (outfile,
-	       "%010d 00000 n \n",
-	       g_array_index (ObjectList, int, curobj));
+	       "%010d 00000 n \n", g_array_index (ObjectList, int, curobj));
     }
 }
 
@@ -1141,15 +561,11 @@ pdf_trailer (int offset, int size, int root)
   char text[255];
 
   sprintf (text,
-	   "\ntrailer\n" \
-	   "\t<<\n" \
-	   "\t\t/Size %d\n" \
-	   "\t\t/Root %d 0 R\n" \
-	   "\t>>\n" \
-	   "startxref\n" \
-	   "%d\n" \
-	   "%%%%EOF\n",
-	   size, root, offset);
+	   "\ntrailer\n"
+	   "\t<<\n"
+	   "\t\t/Size %d\n"
+	   "\t\t/Root %d 0 R\n"
+	   "\t>>\n" "startxref\n" "%d\n" "%%%%EOF\n", size, root, offset);
 
   fprintf (outfile, "%s", text);
 }
@@ -1165,7 +581,7 @@ pdf_process_char (char character, int flush)
 
   if (character == '(' || character == ')')
     {
-      byteswritten = pdf_process_char('\\', 0);
+      byteswritten = pdf_process_char ('\\', 0);
     }
 
   if (bufloc >= 247 || flush == 1)
