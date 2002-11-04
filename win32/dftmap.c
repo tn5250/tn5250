8d9e9ed7754e0885c7d2c215fa7d4da8d0e5da5f
klemscot <klemscot>
2002-11-04 08:54:33 +0000

    Added suport for a Win32 'options' file on install media.

--- win32/dftmap.c
+++ win32/dftmap.c
@@ -30,6 +30,16 @@
 #include "tn5250-private.h"
 #include "resource.h"
 #include <sys/stat.h>
+#include <shlobj.h>
+#include <string.h>
+
+void use_options_file(const char *fn, FILE *rcfile, const char *path);
+HRESULT CreateLink(LPCSTR lpszPathObj, LPCSTR lpszArgs,
+    LPSTR lpszPathLink, LPSTR lpszDesc);
+HRESULT GetShellFolderPath(int nFolder, LPSTR lpszShellPath);
+void install_icon(const char *buf, const char *path);
+void copyrcfile(FILE *optf, FILE *rcfile);
+
 
 char *maplist[] = {
   "United States (CCSID 37)",
@@ -73,11 +83,13 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, PSTR cmdLine, int iCmdShow)
 
    int ccsid;
    FILE *tn5250rc;
-   char *fn;
+   char *fn, *optfn;
    char *quesfmt, *ques;
    struct stat discard;
    int screenwidth, screenheight;
    int replace;
+   int use_opt;
+
 
  /* setup program will pass an arg of the pathname to the install dir */
 
@@ -85,8 +97,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, PSTR cmdLine, int iCmdShow)
         MessageBox(NULL, "Invalid command-line for DFTMAP", "Setup", MB_OK);
         exit(255);
    }
-
-
+  
  /* calculate the filename of tn5250rc */
 
    fn = malloc(strlen(cmdLine) + strlen("\\tn5250rc") + 1);
@@ -95,7 +106,23 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, PSTR cmdLine, int iCmdShow)
    sprintf(fn, "%s\\tn5250rc", cmdLine);
 
 
- /* if the file exists, should we mess with it? */
+/* calculate the filename of options */
+
+   optfn = malloc(strlen(cmdLine) + strlen("\\options") + 1);
+   if (optfn==NULL) 
+       exit(255);
+   sprintf(optfn, "%s\\options", cmdLine);
+
+
+/* check if options file exists */
+
+   use_opt = 0;
+   if (stat(optfn, &discard)==0) {
+      use_opt = 1;
+   }
+
+
+ /* if the tn5250rc file exists, should we mess with it? */
 
    if (stat(fn, &discard)==0) {
        quesfmt = "You already have a TN5250 configuration file called\r\n"
@@ -107,6 +134,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, PSTR cmdLine, int iCmdShow)
                             MB_YESNO|MB_ICONQUESTION|MB_DEFBUTTON2);
        free(ques);
        if (replace != IDYES) {
+           free(optfn);
            free(fn);
            exit(0);
        }
@@ -117,6 +145,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, PSTR cmdLine, int iCmdShow)
 
    ccsid = DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG_DFTMAP), NULL, DlgProc);
    if (ccsid < 1) {
+        free(optfn);
         free(fn);
         exit(1);
    }
@@ -137,10 +166,13 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, PSTR cmdLine, int iCmdShow)
        fprintf(tn5250rc, "   host = ssl:63.167.147.10\n");
        fprintf(tn5250rc, "}\n");
 #endif
+       if (use_opt) 
+            use_options_file(optfn, tn5250rc, cmdLine);
        fclose(tn5250rc);
    }
 
    free(fn);
+   free(optfn);
 
 return 0;
 }
@@ -202,4 +234,271 @@ BOOL CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
      
      return FALSE;
 
+}
+
+void use_options_file(const char *fn, FILE *rcfile, const char *path) {
+
+    char buf[1024];
+    FILE *optf;
+    int first;
+
+    optf = fopen(fn, "r");
+    if (optf==NULL) {
+         MessageBox(NULL, "Unable to open Options file!", "Setup", MB_OK);
+         return;
+    }
+
+    first=1;
+    while (fgets(buf, 1024, optf)!= NULL) {
+
+        if ((*buf)=='\0' || (*buf)=='#') {
+             /* skip these lines */
+        }
+        else if (first) {
+             char msg[2048];
+             sprintf(msg, "This installation has been customized for\n"
+                      "\t%s\n"
+                      "Do you want to install these customizations?",
+                      buf);
+             if (MessageBox(NULL, msg, "Tn5250 Setup", MB_YESNO)!=IDYES)  {
+                  fclose(optf);
+                  return;
+             }
+             first = 0;
+        }
+        else if (!strncmp(buf, "icon:", 5)) {
+             install_icon(buf, path);
+        }
+        else if (!strncmp(buf, "tn5250rc:", 9)) {
+             copyrcfile(optf, rcfile);
+        }
+    }
+
+    fclose(optf);
+    return;
+}
+
+void install_icon(const char *buf, const char *path) {
+     char *mybuf, *profile;
+     char *location, *type, *name;
+     char objpath[MAX_PATH], iconpath[MAX_PATH];
+     char pgmname[20];
+     int x;
+
+     OleInitialize(NULL);   
+
+     mybuf = malloc(strlen(buf)) + 1;
+     if (mybuf == NULL) {
+         MessageBox(NULL, "Out of Memory!", "Setup", MB_OK);
+         exit (1);
+     }
+
+     strcpy(mybuf, buf);
+     if ((name = strchr(mybuf, '\n'))!=NULL) *name = '\0';
+     if ((name = strchr(mybuf, '\r'))!=NULL) *name = '\0';
+
+     profile = mybuf + 6;
+
+     location = strchr(profile, '\\');
+     if (location == NULL) {
+         MessageBox(NULL, "Invalid icon specification!", "Setup", MB_OK);
+         free(mybuf);
+         OleUninitialize();
+         return;
+     }
+
+     *location = '\0';
+     location++;
+
+     type = strchr(location, '\\');
+     if (type == NULL) {
+         MessageBox(NULL, "Invalid icon specification!", "Setup", MB_OK);
+         free(mybuf);
+         OleUninitialize();
+         return;
+     }
+     
+     *type = '\0';
+     type++;
+
+     name = strchr(type, '\\');
+     if (name == NULL) {
+         MessageBox(NULL, "Invalid icon specification!", "Setup", MB_OK);
+         free(mybuf);
+         OleUninitialize();
+         return;
+     }
+     
+     *name = '\0';
+     name++;
+
+     for (x=0; x<strlen(location); x++) 
+          location[x] = tolower(location[x]);
+
+     for (x=0; x<strlen(type); x++) 
+          type[x] = tolower(type[x]);
+
+     if (strcmp(location, "desktop")==0) {
+         GetShellFolderPath(CSIDL_DESKTOPDIRECTORY, iconpath);
+     } else if (strcmp(location, "start menu")==0) {
+         GetShellFolderPath(CSIDL_STARTMENU, iconpath);
+     } else if (strcmp(location, "programs")==0) {
+         GetShellFolderPath(CSIDL_PROGRAMS, iconpath);
+     } else {
+         free(mybuf);
+         MessageBox(NULL, "Invalid icon location!", "Setup", MB_OK);
+         OleUninitialize();
+         return;
+     }
+
+     if (strcmp(type, "printer")==0
+          || strcmp(type, "print")==0
+          || strcmp(type, "lp5250")==0
+          || strcmp(type, "lp5250d")==0)
+         strcpy(pgmname, "LP5250D.EXE");
+     else if (strcmp(type, "display")==0
+          || strcmp(type, "terminal")==0
+          || strcmp(type, "tn5250")==0)
+         strcpy(pgmname, "TN5250.EXE");
+     else {
+         free(mybuf);
+         MessageBox(NULL, "Invalid icon location!", "Setup", MB_OK);
+         OleUninitialize();
+         return;
+     }
+          
+     strcat(iconpath, "\\");
+     strcat(iconpath, name);
+     strcat(iconpath, ".lnk");
+
+     strcpy(objpath, path);
+     strcat(objpath, "\\");
+     strcat(objpath, pgmname);
+
+     if (CreateLink(objpath, profile, iconpath, "TN5250 Shortcut")!=S_OK) {
+         MessageBox(NULL, "CreateLink failed", "Setup", MB_OK);
+     }
+  
+     free(mybuf);
+
+     OleUninitialize();
+     return;
+}
+
+void copyrcfile(FILE *optf, FILE *rcfile) {
+     char buf[1026];
+     while (fgets(buf, 1024, optf)!=NULL) 
+          fputs(buf, rcfile);
+}
+
+
+/*******
+ **  CreateLink(ObjectPath, Args, LinkPath, Description)
+ **
+ **   Creates a Windows link (ShortCut) 
+ **
+ **     ObjectPath = path to program/object you want to link to
+ **           Args = arguments to put on command-line to ObjectPath
+ **       LinkPath = where the link gets put 
+ **    Description = description of link
+ **
+ **  Returns S_OK upon success...  anything else is an error
+ ********/
+
+HRESULT CreateLink(LPCSTR lpszPathObj, LPCSTR lpszArgs,
+    LPSTR lpszPathLink, LPSTR lpszDesc)
+{
+    HRESULT hres;
+    IShellLinkA *psl;
+    char msg[1024];
+
+    // Get a pointer to the IShellLink interface.
+    hres = CoCreateInstance(&CLSID_ShellLink, NULL,
+        CLSCTX_INPROC_SERVER, &IID_IShellLink, (LPVOID *) &psl);
+
+    if (SUCCEEDED(hres)) {
+        IPersistFile* ppf;
+
+        // Set the path to the shortcut target, and add the
+        // description.
+        psl->lpVtbl->SetPath(psl, lpszPathObj);
+        psl->lpVtbl->SetArguments(psl, lpszArgs);
+
+        psl->lpVtbl->SetDescription(psl, lpszDesc);
+
+       // Query IShellLink for the IPersistFile interface for saving the
+       // shortcut in persistent storage.
+        hres = psl->lpVtbl->QueryInterface(psl, &IID_IPersistFile,
+            (LPVOID *) &ppf);
+
+        if (SUCCEEDED(hres)) {
+            WORD wsz[MAX_PATH];
+
+            // Ensure that the string is ANSI.
+            MultiByteToWideChar(CP_ACP, 0, lpszPathLink, -1,
+                wsz, MAX_PATH);
+
+            // Save the link by calling IPersistFile::Save.
+            hres = ppf->lpVtbl->Save(ppf, wsz, TRUE);
+            ppf->lpVtbl->Release(ppf);
+            
+            if (hres != S_OK) {
+               sprintf(msg, "SaveLink: Unknown failure (0x%X)", hres);
+               MessageBox(NULL, msg, "Setup", MB_OK);
+            }
+        }
+        else {
+          sprintf(msg, "QueryInterface: Unknown failure (0x%X)", hres);
+          MessageBox(NULL, msg, "Setup", MB_OK);
+        }
+        psl->lpVtbl->Release(psl);
+    }
+    else {
+        if (hres == REGDB_E_CLASSNOTREG) {
+          MessageBox(NULL, "CoCreateInstance: Class Not Registered", 
+                     "Setup", MB_OK);
+        } else if (hres == CLASS_E_NOAGGREGATION) {
+          MessageBox(NULL, "CoCreateInstance: Class cannot be part of an"
+                    " Aggregate", "Setup", MB_OK);
+        } else {
+          sprintf(msg, "CoCreateInstance: Unknown failure (0x%X)", hres);
+          MessageBox(NULL, msg, "Setup", MB_OK);
+        }
+    }
+    return hres;
+}
+
+
+/******
+ **  GetShellFolderPath(Folder, Path):
+ **     Get a Windows Path.   **
+ **  Folder = AppData, Cache, Cookies, Desktop, Favorites, Fonts, History,
+ **            NetHood, Personal, Printhood, Programs, Recent, SendTo,
+ **            Start Menu, Startup, Templates or ShellNew
+ **
+ **    Path = Returned path
+ **
+ **  Returns S_OK for success, S_FALSE otherwise
+ *******/   
+
+HRESULT GetShellFolderPath(int nFolder, LPSTR lpszShellPath) {
+
+  LPITEMIDLIST pidl;
+  LPMALLOC     pShellMalloc;
+  char         szDir[MAX_PATH];
+  HRESULT      hres;
+
+  hres = S_FALSE;
+  if (SUCCEEDED(SHGetMalloc(&pShellMalloc))) {
+      if (SUCCEEDED(SHGetSpecialFolderLocation(NULL, nFolder, &pidl))) {
+          if (SHGetPathFromIDList(pidl, szDir)) {         
+                strcpy(lpszShellPath, szDir);
+                hres = S_OK;
+          }
+          pShellMalloc->lpVtbl->Free(pShellMalloc, pidl);
+      }
+      pShellMalloc->lpVtbl->Release(pShellMalloc);
+  }
+	
+  return hres;
 }
