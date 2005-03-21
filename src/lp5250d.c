f3b56f3bfb0923f0bb015e9f50c5b5143bb8d0fa
James Rich <james@eaerich.com>
2005-03-21 23:26:43 +0000

    Fix setuid() bug - check the command line options and set the UID before anything else

--- src/lp5250d.c
+++ src/lp5250d.c
@@ -31,17 +31,16 @@
 #include <getopt.h>
 #endif
 
-static void syntax(void);
+static void syntax (void);
 
 /* This just checks what arguments (options) were passed to lp5250d on the
  * command line.
  */
-int check_options (Tn5250Config * tn5250config, int argc, char **argv,
-		   char **user);
+int check_options (int argc, char **argv, char **user);
 
 /* This sets the current UID to the uid of the user passed.
  */
-int change_user (char * user);
+int change_user (char *user);
 
 extern char *version_string;
 
@@ -49,17 +48,12 @@ Tn5250PrintSession *printsess = NULL;
 Tn5250Stream *stream = NULL;
 Tn5250Config *config = NULL;
 
-int main(int argc, char *argv[])
+int
+main (int argc, char *argv[])
 {
   char *user = NULL;
 
-   config = tn5250_config_new ();
-   if (tn5250_config_load_default (config) == -1) {
-        tn5250_config_unref(config);
-        exit (1);  
-   }
-
-  if (check_options (config, argc, argv, &user) != 0)
+  if (check_options (argc, argv, &user) != 0)
     {
       tn5250_config_unref (config);
       exit (1);
@@ -75,88 +69,118 @@ int main(int argc, char *argv[])
 	}
     }
 
-   if (tn5250_config_parse_argv (config, argc, argv) == -1) {
-       tn5250_config_unref(config);
-       syntax();  
-   }
-
-   if (tn5250_config_get (config, "help")) 
-       syntax();
-   else if (tn5250_config_get (config, "version")) {
-       printf ("tn5250 version %s\n", version_string);
-       exit (0);
-   }
-   else if (!tn5250_config_get (config, "host"))   
-       syntax();
-
-
-    if (tn5250_daemon(0,0,0) < 0)
+  config = tn5250_config_new ();
+  if (tn5250_config_load_default (config) == -1)
     {
-        perror("tn5250_daemon");
-        exit(2);
-    } 
+      tn5250_config_unref (config);
+      exit (1);
+    }
+
+  if (tn5250_config_parse_argv (config, argc, argv) == -1)
+    {
+      tn5250_config_unref (config);
+      syntax ();
+    }
+
+  if (tn5250_config_get (config, "help"))
+    {
+      syntax ();
+    }
+  else if (tn5250_config_get (config, "version"))
+    {
+      printf ("tn5250 version %s\n", version_string);
+      exit (0);
+    }
+  else if (!tn5250_config_get (config, "host"))
+    {
+      syntax ();
+    }
+
+
+  if (tn5250_daemon (0, 0, 0) < 0)
+    {
+      perror ("tn5250_daemon");
+      exit (2);
+    }
 
 #ifndef NDEBUG
-   if(tn5250_config_get (config, "trace")) {
-      tn5250_log_open(tn5250_config_get (config, "trace"));
-      TN5250_LOG(("lp5250d version %s, built on %s\n", version_string, 
-            __DATE__));
-      TN5250_LOG(("host = %s\n", tn5250_config_get (config, "host")));
-   }
-#endif 
-
-    openlog("lp5250d", LOG_PID, LOG_DAEMON);
-
-    stream = tn5250_stream_open (tn5250_config_get(config, "host"), config);
-    if(stream == NULL) {
-       syslog(LOG_INFO, "Couldn't connect to %s", 
-		         tn5250_config_get (config,"host"));
-       exit(1);
+  if (tn5250_config_get (config, "trace"))
+    {
+      tn5250_log_open (tn5250_config_get (config, "trace"));
+      TN5250_LOG (("lp5250d version %s, built on %s\n", version_string,
+		   __DATE__));
+      TN5250_LOG (("host = %s\n", tn5250_config_get (config, "host")));
     }
-       
-    printsess = tn5250_print_session_new();
-    tn5250_stream_setenv(stream, "TERM", "IBM-3812-1");
+#endif
+
+  openlog ("lp5250d", LOG_PID, LOG_DAEMON);
+
+  stream = tn5250_stream_open (tn5250_config_get (config, "host"), config);
+  if (stream == NULL)
+    {
+      syslog (LOG_INFO, "Couldn't connect to %s",
+	      tn5250_config_get (config, "host"));
+      exit (1);
+    }
+
+  printsess = tn5250_print_session_new ();
+  tn5250_stream_setenv (stream, "TERM", "IBM-3812-1");
 /*    tn5250_stream_setenv(stream, "DEVNAME", sessionname); */
-    tn5250_stream_setenv(stream, "IBMFONT", "12");
-    syslog(LOG_INFO, "DEVNAME = %s", tn5250_stream_getenv(stream, "DEVNAME"));
-    if (tn5250_stream_getenv(stream, "IBMMFRTYPMDL")) {
-       syslog(LOG_INFO, "TRANSFORM = %s", 
-                     tn5250_stream_getenv(stream, "IBMMFRTYPMDL"));
-       tn5250_stream_setenv(stream, "IBMTRANSFORM", "1");
-    } else
-       tn5250_stream_setenv(stream, "IBMTRANSFORM", "0");
-    tn5250_print_session_set_fd(printsess, tn5250_stream_socket_handle(stream));
-    tn5250_print_session_set_stream(printsess, stream);
-    if (tn5250_config_get (config, "map")) {
-         tn5250_print_session_set_char_map(printsess, 
-                  tn5250_config_get (config, "map"));
-         tn5250_setenv("TN5250_CCSIDMAP", tn5250_config_get (config, "map"), 0);
-    } else {
-         tn5250_print_session_set_char_map(printsess, "37");
-         tn5250_setenv("TN5250_CCSIDMAP", "37", 0);
+  tn5250_stream_setenv (stream, "IBMFONT", "12");
+  syslog (LOG_INFO, "DEVNAME = %s", tn5250_stream_getenv (stream, "DEVNAME"));
+  if (tn5250_stream_getenv (stream, "IBMMFRTYPMDL"))
+    {
+      syslog (LOG_INFO, "TRANSFORM = %s",
+	      tn5250_stream_getenv (stream, "IBMMFRTYPMDL"));
+      tn5250_stream_setenv (stream, "IBMTRANSFORM", "1");
     }
-    if (tn5250_config_get (config, "outputcommand")) 
-         tn5250_print_session_set_output_command(printsess, 
-                 tn5250_config_get (config, "outputcommand"));
-    else
-         tn5250_print_session_set_output_command(printsess, 
-                 "scs2ascii|lpr");
+  else
+    {
+      tn5250_stream_setenv (stream, "IBMTRANSFORM", "0");
+    }
+  tn5250_print_session_set_fd (printsess,
+			       tn5250_stream_socket_handle (stream));
+  tn5250_print_session_set_stream (printsess, stream);
+  if (tn5250_config_get (config, "map"))
+    {
+      tn5250_print_session_set_char_map (printsess,
+					 tn5250_config_get (config, "map"));
+      tn5250_setenv ("TN5250_CCSIDMAP", tn5250_config_get (config, "map"), 0);
+    }
+  else
+    {
+      tn5250_print_session_set_char_map (printsess, "37");
+      tn5250_setenv ("TN5250_CCSIDMAP", "37", 0);
+    }
+  if (tn5250_config_get (config, "outputcommand"))
+    {
+      tn5250_print_session_set_output_command (printsess,
+					       tn5250_config_get (config,
+								  "outputcommand"));
+    }
+  else
+    {
+      tn5250_print_session_set_output_command (printsess, "scs2ascii|lpr");
+    }
 
-    tn5250_print_session_main_loop(printsess);
+  tn5250_print_session_main_loop (printsess);
 
-    tn5250_print_session_destroy(printsess);
-    tn5250_stream_destroy (stream);
-    if (config!=NULL)
-          tn5250_config_unref (config);
- #ifndef NDEBUG
-    tn5250_log_close();
-#endif 
-    return 0;
+  tn5250_print_session_destroy (printsess);
+  tn5250_stream_destroy (stream);
+  if (config != NULL)
+    {
+      tn5250_config_unref (config);
+    }
+#ifndef NDEBUG
+  tn5250_log_close ();
+#endif
+  return 0;
 }
 
-static void syntax()
+static void
+syntax ()
 {
-   printf("Usage:  lp5250d [options] host[:port]\n"
+  printf ("Usage:  lp5250d [options] host[:port]\n"
 	  "Options:\n"
 	  "\ttrace=FILE                 specify FULL path to log file\n"
 	  "\tmap=NAME                   specify translation map\n"
@@ -167,14 +191,14 @@ static void syntax()
 	  "\t-v,--version               display version\n"
 	  "\t-H,--help                  display this help\n");
 
-   exit (255);
+  exit (255);
 }
 
 
 /* This checks the options lp5250d was passed.
  */
 int
-check_options (Tn5250Config * tn5250config, int argc, char **argv, char **user)
+check_options (int argc, char **argv, char **user)
 {
 #ifdef HAVE_GETOPT_H
   struct option options[4];
@@ -202,8 +226,7 @@ check_options (Tn5250Config * tn5250config, int argc, char **argv, char **user)
 #endif
 
 #ifdef HAVE_GETOPT_H
-  while ((i =
-	  getopt_long (argc, argv, "u:vH", options, NULL)) != -1)
+  while ((i = getopt_long (argc, argv, "u:vH", options, NULL)) != -1)
 #else
   while ((i = getopt (argc, argv, "u:vH")) != -1)
 #endif
@@ -234,7 +257,7 @@ check_options (Tn5250Config * tn5250config, int argc, char **argv, char **user)
 /* Set the UID to run lp5250d as.
  */
 int
-change_user (char * user)
+change_user (char *user)
 {
   struct passwd *pwent;
 
@@ -254,6 +277,6 @@ change_user (char * user)
       perror ("setuid()");
       return (-1);
     }
-  printf ("Now running as UID %d\n", getuid ());
+
   return (0);
 }
